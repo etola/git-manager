8c976006bee54764d363aa31a38fd3a0b3604fd6
engin tola <engintola@gmail.com>
2014-08-17 12:26:51 +0300

    code refactored for better pad/layout separation

--- git-manager-gui.py
+++ git-manager-gui.py
@@ -2,9 +2,36 @@
 
 import sys, os, curses, re, math
 
-# from utility              import *
-# from gm_resources         import *
 from gm_core              import *
+
+class Layout:
+    def __init__(self):
+        self.xs  = 0      # screen coordinates
+        self.ys  = 0
+        self.xe  = 0
+        self.ye  = 0
+        self.x0  = 0      # starting coordinates for the pad
+        self.y0  = 0
+
+    def update_screen_coords( self, ys, ye, xs, xe ):
+        self.xs = xs
+        self.ys = ys
+        self.xe = xe
+        self.ye = ye
+
+    def refresh( self, pad ):
+        pad.refresh( self.y0, self.x0, self.ys, self.xs, self.ye, self.xe )
+
+    def erase( self ):
+        tmp_pad = curses.newpad( self.ye-self.ys+1, self.xe-self.xs+1 )
+        tmp_pad.refresh( 0, 0, self.ys, self.xs, self.ye, self.xe )
+
+    def h( self ):
+        return self.ye-self.ys+1
+
+    def w( self ):
+        return self.xe-self.xs+1
+
 
 def num_list_elem( l ):
     lsz = len(l)
@@ -56,7 +83,6 @@ def render_text( scr, text, y, x, h, w ):
     y = y+c
     return y, max_len
 
-
 def draw_repo_details( scr, g ):
 
     scr.erase()
@@ -67,7 +93,7 @@ def draw_repo_details( scr, g ):
 
     h,w = scr.getmaxyx()
 
-    y = DY
+    y = 0
     x = 1
     scr.vline(  y, 0, curses.ACS_VLINE, h     )
 
@@ -106,7 +132,29 @@ def draw_repo_details( scr, g ):
     scr.hline ( y+1, x, curses.ACS_HLINE, w-x-1, curses.color_pair(5) )
     y, ml = render_text( scr, g.raw_remotes, y+2, x, h, w )
 
-    # scr.addstr( h-1, w-1, '' )
+def draw_repo_man_screen( scr ):
+    scr.erase()
+    y = 1
+    x = 2
+    h,w = scr.getmaxyx()
+    scr.addstr( y, x, 'Repositories', curses.color_pair(5) )
+    scr.hline( y+1, x, curses.ACS_HLINE, w-2*x, curses.color_pair(5) )
+
+    rpsz = LGD[1][1]
+
+    y = y + 2
+    cnt = 0
+    for repo in rep_names:
+        g = G[cnt]
+        li = 0
+        x = 0
+        if g.repo_name == "":
+            scr.hline( y+cnt, x, curses.ACS_HLINE, w )
+        else:
+            scr.addstr( y+cnt, x,        g.repo_name.rjust( rpsz ), curses.color_pair(5) )
+            scr.addstr( y+cnt, x+rpsz+1, g.path )
+        cnt = cnt+1
+
 
 def draw_legend( scr ):
     y = 1
@@ -116,8 +164,9 @@ def draw_legend( scr ):
     scr.hline ( y+1, x, curses.ACS_HLINE, w-2*x, curses.color_pair(5) )
     scr.addstr( y+2, x+5, 'd'.ljust(5)+': detailed output for repo toggle' )
     scr.addstr( y+3, x+5, 'u'.ljust(5)+': update git reports' )
+    scr.addstr( y+4, x+5, 'r'.ljust(5)+': repository view' )
 
-    y = y + 5
+    y = y + 6
     scr.addstr( y, x, 'Legend', curses.color_pair(5) )
     scr.hline ( y+1, x, curses.ACS_HLINE, w-2*x, curses.color_pair(5) )
 
@@ -146,78 +195,69 @@ def draw_footer( scr, st ):
     scr.addstr( h-2, 1, st,                    curses.color_pair(5) )
 
 def draw_main( scr ):
-
     scr.erase()
-
-    global Y0
-    global X0
-    Y0 = DY
-    X0 = DX
-
     h,w = scr.getmaxyx()
+
+    y = 0
+    x = 0
 
     # draw the legend
     n_legs = len( LGD )
     li = 1
     for leg in LGD:
-        scr.addstr( Y0,   X0,     ''.center(leg[1]), curses.A_STANDOUT )
+        scr.addstr( y,   x,     ''.center(leg[1]), curses.A_STANDOUT )
         if leg[2] == 'Left':
-            scr.addstr( Y0+1, X0, leg[0].ljust(leg[1]), curses.A_STANDOUT )
+            scr.addstr( y+1, x, leg[0].ljust(leg[1]), curses.A_STANDOUT )
         elif leg[2] == 'Center':
-            scr.addstr( Y0+1, X0, leg[0].center(leg[1]), curses.A_STANDOUT )
+            scr.addstr( y+1, x, leg[0].center(leg[1]), curses.A_STANDOUT )
         elif leg[2] == 'Right':
-            scr.addstr( Y0+1, X0, leg[0].rjust(leg[1]), curses.A_STANDOUT )
+            scr.addstr( y+1, x, leg[0].rjust(leg[1]), curses.A_STANDOUT )
         else:
             assert( 0 > 1)
 
-        scr.addstr( Y0+2, X0,     ''.center(leg[1]), curses.A_STANDOUT )
-        X0 = X0+leg[1]+1
+        scr.addstr( y+2, x,     ''.center(leg[1]), curses.A_STANDOUT )
+        x = x+leg[1]+1
         if li != n_legs:
-            scr.vline(  Y0,     X0-1, curses.ACS_VLINE, 3 , curses.A_STANDOUT    )
-            scr.vline(  Y0+3,   X0-1, curses.ACS_VLINE, NR )
+            scr.vline(  y,     x-1, curses.ACS_VLINE, 3 , curses.A_STANDOUT    )
+            scr.vline(  y+3,   x-1, curses.ACS_VLINE, NR )
         li = li+1
 
-    LGSZ = X0-DX-1
-
     # display repo reports
-    Y0 = DY + 3
-    X0 = DX
+    y = y + 3
+
     cnt = 0
     for repo in rep_names:
         g = G[cnt]
         li = 0
-        x = X0
+        x = 0
         if g.repo_name == "":
-            scr.hline( Y0+cnt, x, curses.ACS_HLINE, LGSZ )
+            scr.hline( y+cnt, x, curses.ACS_HLINE, w-2 )
         else:
             for leg in LGD:
                 if li == 0:
-                    scr.addstr( Y0+cnt, x, (str(cnt)+' ').rjust(leg[1]) )
+                    scr.addstr( y+cnt, x, (str(cnt)+' ').rjust(leg[1]) )
                 elif li == 1:
-                    scr.addstr( Y0+cnt, x, (repo+' ').rjust(leg[1]) )
+                    scr.addstr( y+cnt, x, (repo+' ').rjust(leg[1]) )
                 elif li == 2:
-                    scr.addstr( Y0+cnt, x, repo_remote_state(g).center(leg[1]) )
+                    scr.addstr( y+cnt, x, repo_remote_state(g).center(leg[1]) )
                 elif li == 3:
-                    scr.addstr( Y0+cnt, x, num_list_elem(g.sfiles).center(leg[1]) )
+                    scr.addstr( y+cnt, x, num_list_elem(g.sfiles).center(leg[1]) )
                 elif li == 4:
-                    scr.addstr( Y0+cnt, x, num_list_elem(g.cfiles).center(leg[1]) )
+                    scr.addstr( y+cnt, x, num_list_elem(g.cfiles).center(leg[1]) )
                 elif li == 5:
-                    scr.addstr( Y0+cnt, x, num_list_elem(g.ufiles).center(leg[1]) )
+                    scr.addstr( y+cnt, x, num_list_elem(g.ufiles).center(leg[1]) )
                 elif li == 6:
-                    scr.addstr( Y0+cnt, x, g.branch.center(leg[1]) )
+                    scr.addstr( y+cnt, x, g.branch.center(leg[1]) )
                 elif li == 7:
-                    scr.addstr( Y0+cnt, x, var_state(g.commit).center(leg[1]) )
+                    scr.addstr( y+cnt, x, var_state(g.commit).center(leg[1]) )
                 x = x + leg[1]+1
                 li = li+1
         cnt = cnt+1
 
-    scr.move( DY+3+SELR, DX )
+    scr.move( selected_rep_id+3, 0 )
     curr_y, curr_x = scr.getyx()
     scr.chgat( curr_y, curr_x+LGD[0][1]+2, LGD[1][1]-1, curses.color_pair(2) | curses.A_BOLD )
-    scr.move( DY+3+SELR, curr_x + LGD[0][1] + LGD[1][1] +1 )
-    # scr.border(0)
-
-    # scr.addstr( h-1, w-1, '' )
+    scr.move( 3+selected_rep_id, curr_x + LGD[0][1] + LGD[1][1] +1 )
 
 def cache_git_reports():
     global G
@@ -227,20 +267,53 @@ def cache_git_reports():
         c = c+1
 
 
+def update_layouts():
+    global ML, S0, S1, display_mode
+    global main_pad
+    global supp_pad
 
-rep_names = get_repositories()
 
-NR  = len( rep_names )
-G = [None] * NR
+    h,w = stdscr.getmaxyx()
+    if w < 140:
+        display_mode = 'single'
+    else:
+        display_mode = 'split'
 
-mh = max(NR+10,20)    # main pad height
-mw = 75               # main pad width
-sh = 200              # support pad height
-sw_default = 100      # support pad default width
-sw = sw_default       # support pad active width
-lh = 20               # legend pad height
-lw = 80               # legend pad width
-dual_mode = False     # initialially we start with only the main pad
+    if display_mode == 'single':
+        S0.update_screen_coords( 0, h-1, 0, mw     )
+        S1.update_screen_coords( 0, h-1, mw, w-1 )
+
+        if active_screen == 0:
+            main_pad = curses.newpad( pad_h, mw )
+
+            sw = (w-1-mw)/2+1
+            sh = (h-1-(NR+3))/2
+
+            ML.update_screen_coords( max(0,sh), min(h-1,sh+NR+3), max(0,sw), min(w-1,sw+mw) )
+
+        elif active_screen == 1:
+            main_pad = curses.newpad( pad_h, w  )
+            ML.update_screen_coords( 0, h-1, 0, w-1 )
+        elif active_screen == 3:
+            main_pad = curses.newpad( pad_h, w  )
+            ML.update_screen_coords( 0, h-1, 0, w-1 )
+
+    else:
+        ML.update_screen_coords( 0, h-1, 0, w-1 )
+        S0.update_screen_coords( 0, h-1, 0, mw     )
+        S1.update_screen_coords( 0, h-1, mw, w-1 )
+
+        if active_screen == 0 or active_screen == 1 or active_screen == 2:
+            main_pad = curses.newpad( pad_h, mw   )
+            supp_pad = curses.newpad( pad_h, w-mw )
+        elif active_screen == 3:
+            main_pad = curses.newpad( pad_h, w-1 )
+
+    main_pad.keypad(True)
+
+    ML.erase()
+    S0.erase()
+    S1.erase()
 
 #
 # initialize main window legend
@@ -258,128 +331,129 @@ LGD = ( ('ID '           , 4 , 'Right'),
         ('Branch'        , 8 , 'Center'),
         ('Dirty'        , 10, 'Center') )
 
-# start curses
-stdscr = curses.initscr()
-curses.cbreak()
-curses.noecho()
-
-# color setup
-curses.start_color()
-curses.use_default_colors()
-for i in range(0, curses.COLORS):
-    curses.init_pair(i + 1, i, -1)
-
-def update_pad_dimensions():
-    global LY, LX, fw, fh, dual_mode, sw
-
-    fh,fw = stdscr.getmaxyx()
-    LY = 0
-    LX = 0
-    if fw < mw+sw_default:
-        LX = (fw-mw)/2
-        LY = (fh-mh)/2
-        dual_mode = False
-        sw = fw
-    else:
-        LX = 0
-        LY = 0
-        sw = fw - mw -1
-        dual_mode = True
 
 
-DY  =  0
-DX  =  1
-SELR = 0
-ACTIVE_SCREEN = 0
+rep_names = get_repositories()
+NR  = len( rep_names )
+G = [None] * NR
+
+mw = 73
+selected_rep_id = 0
 
 try:
+    stdscr = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
 
-    update_pad_dimensions()
+    curses.start_color()
+    curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
 
-    win_main   = curses.newpad(mh,mw)
-    win_supp   = curses.newpad(sh,sw)
-    win_legend = curses.newpad(lh,lw)
+    h,w = stdscr.getmaxyx()
 
-    dl = 0
-    win_main.keypad(True)
+    pad_h = 200
+
+    main_pad = curses.newpad( pad_h, mw )
+    main_pad.keypad(True)
+    main_pad.border(0)
+    main_pad.addstr(0, 0, 'main' )
+
+    supp_pad = curses.newpad( pad_h, 100 )
+    supp_pad.keypad(True)
+    supp_pad.border(0)
+    supp_pad.addstr(0, 0, 'support' )
+
+
+    ML = Layout()
+    S0 = Layout()
+    S1 = Layout()
+
+    active_screen = 0
+    display_mode = 'single'
+
+    update_layouts()
 
     cache_git_reports()
-    k=0
-    while k != ord('q'):
+    # main_pad.border(0)
+    draw_main( main_pad )
+    draw_repo_details( supp_pad, G[selected_rep_id] )
 
-        win_main.erase()
+    update_main = False
 
-        if k == curses.KEY_DOWN:
-            SELR = SELR + 1
-            if SELR >= NR-1:
-                SELR=NR-1
-            if G[SELR].repo_name == "":
-                SELR=SELR+1
-            if SELR >= NR-1:
-                SELR=NR-1
+
+    k = 0
+    while k!= ord('q'):
+
+        update_support = False
+        update_main    = False
+
+        if k == curses.KEY_RESIZE:
+            update_layouts()
+
+        elif k == curses.KEY_DOWN:
+            selected_rep_id = selected_rep_id + 1
+            if selected_rep_id >= NR-1:
+                selected_rep_id=NR-1
+            if G[selected_rep_id].repo_name == "":
+                selected_rep_id=selected_rep_id+1
+            if selected_rep_id >= NR-1:
+                selected_rep_id=NR-1
 
         elif k == curses.KEY_UP:
-            SELR = SELR - 1
-            if SELR < 0:
-                SELR=0
-            if G[SELR].repo_name == "":
-                SELR=SELR-1
-            if SELR < 0:
-                SELR=0
-
-        elif k == ord('u'):
-            win_main.erase()
-            st = 'Updating Repo Info'
-            win_main.addstr( mh/2, mw/2-len(st)/2, st, curses.color_pair(2) )
-            win_main.refresh(0,0, LY, LX, min(fh-1,LY+mh), min(fw-1,LX+mw) )
-            cache_git_reports()
-
-        elif k == ord('h'):
-            if dl == 1:
-                win_legend.erase()
-                win_legend.refresh(0, 0, max(0,fh/2-lh/2), max(0,fw/2-lw/2), min(fh-1,fh/2+lh/2), min(fw-1,fw/2+lw/2) )
-            dl = (dl+1)%2
-
-        elif k == ord('d'):
-            if dual_mode is False:
-                ACTIVE_SCREEN = (ACTIVE_SCREEN+1)%2
-                stdscr.erase()
-                stdscr.refresh()
-            else:
-                ACTIVE_SCREEN = 0
-
-            update_pad_dimensions()
-            win_supp = curses.newpad(sh,sw)
+            selected_rep_id = selected_rep_id - 1
+            if selected_rep_id < 0:
+                selected_rep_id=0
+            if G[selected_rep_id].repo_name == "":
+                selected_rep_id=selected_rep_id-1
+            if selected_rep_id < 0:
+                selected_rep_id=0
 
 
-        elif k == curses.KEY_RESIZE:
-            stdscr.erase()
-            stdscr.refresh()
-            update_pad_dimensions()
-            win_supp = curses.newpad(sh,sw)
+        elif k == ord('d') and display_mode == 'single':
+            if active_screen == 1:
+                active_screen = 0
+            elif active_screen == 0:
+                active_screen = 1
+            update_layouts()
 
-        if ACTIVE_SCREEN == 0:
-            draw_main( win_main )
-            if dual_mode is False:
-                draw_footer( win_main, 'Press d for detailed view: ['+G[SELR].repo_name +']' )
-            win_main.refresh(0, 0, LY, LX, min(fh-1,LY+mh), min(fw-1,LX+mw) )
-            if fw > mw + sw:
-                draw_repo_details( win_supp, G[SELR] )
-                win_supp.refresh( 0, 0, LY, LX+mw, min(fh-1,LY+sh-1), min(fw-1,LX+mw+sw-1)  )
-            else:
-                win_supp.erase()
-                win_supp.refresh( 0, 0, LY, LX+mw, min(fh-1,LY+sh-1), min(fw-1,LX+mw+sw-1)  )
-        elif ACTIVE_SCREEN == 1:
-            draw_repo_details( win_supp, G[SELR] )
-            win_supp.refresh( 0, 0, 0, 0, min(fh-1,sh-1), min(fw-1,sw-1)  )
+        elif k == ord('r'):
+            active_screen = 3
+            update_layouts()
 
-        if dl == 1:
-            draw_legend( win_legend )
-            win_legend.refresh(0, 0, max(0,fh/2-lh/2), max(0,fw/2-lw/2), min(fh-1,fh/2+lh/2), min(fw-1,fw/2+lw/2) )
+        elif k == ord('m'):
+            active_screen = 0
+            update_layouts()
 
-        k = win_main.getch()
 
-    pass
+        if display_mode == 'single':
+
+            if active_screen == 0:
+                draw_main( main_pad )
+                ML.refresh( main_pad )
+            elif active_screen == 1:
+                draw_repo_details( main_pad, G[selected_rep_id] )
+                ML.refresh( main_pad )
+            elif active_screen == 3:
+                draw_repo_man_screen( main_pad )
+                ML.refresh( main_pad )
+            elif active_screen == 2:
+                print 'cannot draw two pads in one small screen'
+                print 'should not have entered this loop'
+                assert( 0 > 1 )
+
+        elif display_mode == 'split':
+
+            if active_screen == 2 or active_screen == 0 or active_screen == 1:
+                draw_main( main_pad )
+                draw_repo_details( supp_pad, G[selected_rep_id] )
+                S0.refresh( main_pad  )
+                S1.refresh( supp_pad )
+            elif active_screen == 3:
+                draw_repo_man_screen( main_pad )
+                ML.refresh( main_pad )
+
+        k = main_pad.getch()
 
 finally:
     curses.nocbreak()
